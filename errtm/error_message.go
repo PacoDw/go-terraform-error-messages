034f8a2928e40c6d93e6d48ecd2b23f6c690c4b0
PacoDw <__pm@outlook.com>
2020-07-27 10:49:51 -0500

    :fire: implemented partial mode and refactored code

--- errtm/error_message.go
+++ errtm/error_message.go
@@ -3,6 +3,7 @@ package errtm
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/spf13/cast"
@@ -36,36 +37,74 @@ type Config struct {
 	// create a message error, so this will avoid modify the values of the configuration error that were saved/set
 	// with the Save methods
 	// if the user want o use just one method set to create an error avoiding extra info, this avoid it.
-	reConfig *Config
+	storageConfig *Config
+
+	// storageConfig all the Save's methods storage the data in this attribute, so the first level of methods will remplace to
+	// volatil storage I mean will be storageConfig
+
+	partialConfig *Config
+
+	copyConfig *Config
+
+	PartialMode bool
+}
+
+// EnablePartialMode ...
+func (c *Config) EnablePartialMode(pm bool) *Config {
+	c.PartialMode = pm
+
+	if !pm {
+		c.partialConfig = &Config{}
+		return c
+	}
+
+	c.copyConfig = c
+
+	return c.partialConfig
 }
 
 // SaveID saves the ID in the template configuration to be used in all error messages
 func (c *Config) SaveID(id string) *Config {
-	c.ID = id
+	if c.storageConfig == nil {
+		c.storageConfig = &Config{}
+	}
+	c.storageConfig.ID = id
 	return c
 }
 
 // SaveProviderName saves a provider name in the template configuration to be used in all error messages
 func (c *Config) SaveProviderName(pn string) *Config {
-	c.ProviderName = pn
+	if c.storageConfig == nil {
+		c.storageConfig = &Config{}
+	}
+	c.storageConfig.ProviderName = pn
 	return c
 }
 
 // SaveResourceName saves a resource name in the template configuration to be used in all error messages
 func (c *Config) SaveResourceName(rn string) *Config {
-	c.ResourceName = rn
+	if c.storageConfig == nil {
+		c.storageConfig = &Config{}
+	}
+	c.storageConfig.ResourceName = rn
 	return c
 }
 
 // SaveError saves a error message gave by the API into the template configuration to be used in all error messages
 func (c *Config) SaveError(e interface{}) *Config {
-	c.Error = cast.ToString(e)
+	if c.storageConfig == nil {
+		c.storageConfig = &Config{}
+	}
+	c.storageConfig.Error = cast.ToString(e)
 	return c
 }
 
 // SaveAttribute saves the attribute that does't set correctly in terraform into the template configuration to be used in all error messages
 func (c *Config) SaveAttribute(a string) *Config {
-	c.Attribute = a
+	if c.storageConfig == nil {
+		c.storageConfig = &Config{}
+	}
+	c.storageConfig.Attribute = a
 	return c
 }
 
@@ -74,75 +113,79 @@ func (c *Config) SaveAttribute(a string) *Config {
 // it occurs, we recommend use one of the follows const:
 // Creating, Reading, Updating, Deleting or Setting
 func (c *Config) SaveState(a errState) *Config {
-	c.State = a
+	if c.storageConfig == nil {
+		c.storageConfig = &Config{}
+	}
+	c.storageConfig.State = a
 	return c
 }
 
 // SetID sets the ID as a temporaly value to create one message error
 func (c *Config) SetID(id string) *Config {
-	if c.reConfig == nil {
-		c.reConfig = &Config{}
-	}
-	c.reConfig.SaveID(id)
+	c.ID = id
 	return c
 }
 
 // SetProviderName sets a provider name of the current provider
 func (c *Config) SetProviderName(pn string) *Config {
-	if c.reConfig == nil {
-		c.reConfig = &Config{}
-	}
-	c.reConfig.SaveProviderName(pn)
+	c.ProviderName = pn
 	return c
 }
 
 // SetResourceName sets a resource name
 func (c *Config) SetResourceName(rn string) *Config {
-	if c.reConfig == nil {
-		c.reConfig = &Config{}
-	}
-	c.reConfig.SaveResourceName(rn)
+	c.ResourceName = rn
 	return c
 }
 
 // SetError sets a error message gave by the API
 func (c *Config) SetError(e interface{}) *Config {
-	if c.reConfig == nil {
-		c.reConfig = &Config{}
-	}
-	c.reConfig.SaveError(e)
+	c.Error = cast.ToString(e)
 	return c
 }
 
 // SetAttribute a attribute that does't set correctly in terraform
 func (c *Config) SetAttribute(a string) *Config {
-	if c.reConfig == nil {
-		c.reConfig = &Config{}
-	}
-	c.reConfig.SaveAttribute(a)
+	c.Attribute = a
 	return c
 }
 
 // SetState sets an error type, this depending on which method/circumstance
 // it occurs, we recommend use one of the follows const:
 // Creating, Reading, Updating, Deleting or Setting
-func (c *Config) SetState(a errState) *Config {
-	if c.reConfig == nil {
-		c.reConfig = &Config{}
-	}
-	c.reConfig.SaveState(a)
+func (c *Config) SetState(s errState) *Config {
+	c.State = s
 	return c
 }
 
 // ToError builds and returns the error message
-// Note: if you have created a previous Conf Error to used in all the message
+// Note: if you have created a previous Conf Error to be used in all the message
 // and later is used the Set's Method, it will create a new message error
 // copy first the Configuration Error and ovewrite it with the Temporaly configuration Error
 func (c *Config) ToError() error {
-	if c.reConfig != nil {
-		return c.FillMessage(c.reConfig)
+	var temp Config = *c
+
+	if c.PartialMode {
+		c = c.copyConfig
+		temp = *c.partialConfig
 	}
-	return NewErrorMessage(c)
+
+	if !reflect.DeepEqual(c.storageConfig, &Config{}) {
+		temp = *fillMessage(c.storageConfig, &temp)
+	}
+
+	c.cleanConfig()
+
+	return NewErrorMessage(&temp)
+}
+
+func (c *Config) cleanConfig() {
+	c.ID = ""
+	c.ProviderName = ""
+	c.ResourceName = ""
+	c.Error = ""
+	c.Attribute = ""
+	c.State = ""
 }
 
 func fillMessage(c, newConfig *Config) *Config {
@@ -177,25 +220,33 @@ func fillMessage(c, newConfig *Config) *Config {
 
 // FillMessage sets the missing config attributes and returns the error message
 func (c *Config) FillMessage(config *Config) error {
-	temp := fillMessage(fillMessage(c, c.reConfig), config)
-	c.reConfig = &Config{}
+	var temp Config = *c
 
-	return NewErrorMessage(temp)
+	if c.PartialMode {
+		c = c.copyConfig
+		temp = *c.partialConfig
+	}
+
+	t := fillMessage(fillMessage(c.storageConfig, &temp), config)
+
+	c.cleanConfig()
+
+	return NewErrorMessage(t)
 }
 
 // SaveProviderName retunrs a Config struct setting the provider name
 func SaveProviderName(pn string) *Config {
-	return &Config{ProviderName: pn, reConfig: &Config{}}
+	return &Config{storageConfig: &Config{ProviderName: pn}}
 }
 
 // SetError retunrs a Config struct setting the provider name
 func SetError(err string) *Config {
-	return &Config{reConfig: &Config{Error: err}}
+	return &Config{Error: err, storageConfig: &Config{}}
 }
 
 // NewConfigurationError creates a new Configuration Error
 func NewConfigurationError() *Config {
-	return &Config{reConfig: &Config{}}
+	return &Config{storageConfig: &Config{}}
 }
 
 // NewErrorMessage builds and creates the error message returning it as error type
